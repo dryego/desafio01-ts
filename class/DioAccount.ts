0ba801f9664d37fcd576ffb0d94f6c062c115d62
dryego barbosa <dryegobarbosa@MacBook-Air-de-dryego.local>
2024-02-15 11:16:11 -0300

    adicionado os metodos deposit(depositar) e withdraw(sacar) na class DioAccount.

--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,42 +1,58 @@
 export abstract class DioAccount {
-  private name: string
-  private readonly accountNumber: number
-  balance: number = 0
-  private status: boolean = true
+  private name: string;
+  private readonly accountNumber: number;
+  balance: number = 0;
+  private status: boolean = true;
 
-  constructor(name: string, accountNumber: number){
-    this.name = name
-    this.accountNumber = accountNumber
+  constructor(name: string, accountNumber: number) {
+    this.name = name;
+    this.accountNumber = accountNumber;
   }
 
   setName = (name: string): void => {
-    this.name = name
-    console.log('Nome alterado com sucesso!')
-  }
+    this.name = name;
+    console.log("Nome alterado com sucesso!");
+  };
 
   getName = (): string => {
-    return this.name
-  }
+    return this.name;
+  };
 
-  deposit = (): void => {
-    if(this.validateStatus()){
-      console.log('Voce depositou')
+  deposit = (depositAmount: number): string => {
+    if (this.validateStatus() && depositAmount > 0) {
+      this.balance += depositAmount;
+      return "Voce depositou";
     }
-  }
+    return "Valor invalido.";
+  };
 
-  withdraw = (): void => {
-    console.log('Voce sacou')
-  }
+  withdraw = (withdrawalValue: number): string => {
+    switch (true) {
+      case !this.validateStatus():
+        return "Conta invalida.";
+        break;
+      case withdrawalValue > this.balance:
+        return "Saudo insuficiente.";
+        break;
+      case withdrawalValue === 0:
+        return "Valor invalido.";
+        break;
+      default:
+        this.balance -= withdrawalValue;
+        return "Você sacou.";
+        break;
+    }
+  };
 
-  getBalance = (): void => {
-    console.log(this.balance)
-  }
+  getBalance = (): number => {
+    return this.balance;
+  };
 
   private validateStatus = (): boolean => {
     if (this.status) {
-      return this.status
+      return this.status;
     }
 
-    throw new Error('Conta inválida')
-  }
+    throw new Error("Conta inválida");
+  };
 }
